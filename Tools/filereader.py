9ac96f28f057ae3171cdc71056067d859e9f98dd
Prathyush Metikal <prathyushmetikalgit@github.com>
2026-03-15 21:44:59 +0530

    initial create

---
+++ Tools/filereader.py
@@ -0,0 +1,41 @@
+import os
+import docx
+import pdfplumber
+import subprocess
+
+def extract_cv_text(file_path):
+    ext=os.path.splitext(file_path)[-1].lower()
+
+    if ".docx" in ext:
+        try:
+            doc=docx.Document(file_path)
+            text=[para.text for para in doc.paragraphs]
+            return '\n'.join(text)
+        except Exception as e:
+            return f"Error reading the .doc file: {e}"
+    elif ".pdf" in ext:
+        try:
+            text=[]
+            with pdfplumber.open(file_path) as pdf:
+                for page in pdf.pages:
+                    page_text=page.extract_text()
+                    if page_text:
+                        text.append(page_text)
+            return '\n'.join(text)
+        except Exception as e:
+            return f"Error reading the .pdf file: {e}"
+    elif ".doc" in ext:
+        try:
+            temp_docx=file_path+".temp.docx"
+            subprocess.run(['soffice','--headless','docx','--outdir',os.path.dirname(file_path),file_path],check=True)
+            doc=docx.Document(os.path.splitext(file_path)[0]+".docx")
+            text=[para.text for para in doc.paragraphs]
+            os.remove(os.path.splitext(file_path)[0]+".docx")
+            return '\n'.join(text)
+        
+        except Exception as e:
+            return f"Error reading .doc file : {e}"
+    else:
+        return " Unsupported file format!! Please use .pdf,.doc or .docx"
+    
+    
---
+++ Tools/testfilter.py
@@ -0,0 +1,32 @@
+from Tools.filereader import extract_cv_text
+
+filename="./Docs/just-text.pdf"
+text=extract_cv_text(filename)
+
+
+
+if __name__ == "__main__":
+    filename="./Docs/just-text.pdf"
+    text=extract_cv_text(filename)
+    print(text)
+
+
+#  uv run python -m Tools.testfilter
+# >>
+# Sample PDF File for Testing & Practice
+# Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id ligula ac quam aliquet
+# venenatis. Ut at ligula tincidunt, sollicitudin purus nec, blandit metus. Sed auctor
+# venenatis purus, et venenatis ipsum. Nulla facilisi. Pellentesque habitant morbi tristique
+# senectus et netus et malesuada fames ac turpis egestas. Proin bibendum purus nisl, at
+# volutpat risus sagittis at. Sed gravida feugiat libero, et placerat libero aliquet eget. Nam
+# non risus sit amet felis accumsan tristique. Nulla facilisi. Curabitur sit amet odio odio.
+# Nunc nec bibendum eros, nec convallis ligula.
+# Vivamus bibendum elit sed massa feugiat, in tristique eros vestibulum. Integer at erat
+# volutpat, vestibulum enim eu, ultricies justo. Praesent lacinia venenatis tellus vel
+# congue. Curabitur non libero id urna mattis eleifend. Nulla facilisi. Donec sed eleifend
+# massa. Fusce nec ex vel odio lacinia egestas. Suspendisse sed ex ut urna tincidunt
+# semper at in ex. Vestibulum et justo eget orci posuere rhoncus. Sed vel enim euismod,
+# sollicitudin urna nec, fermentum massa. Nulla facilisi. Donec gravida ligula libero, id
+# dapibus ligula vulputate a. Nulla facilisi.
+# You can get more free sample data at https://www.slingacademy.com/cat/sample-data/
+# Happy coding & have a nice day!
